0c2455328e090c285b81d2a8e5eeb215493ffd48
brits <joseluizbrits@gmail.com>
2024-05-09 20:34:57 -0300

    style: add margin to the blog image for desktop large screen

--- src/slices/BlogImage/styles.ts
+++ src/slices/BlogImage/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ImageWrapper = styled.div`
   max-width: 780px;
   aspect-ratio: 16/9;
-  margin: 0 auto;
+  margin: 0 40px;
   margin-top: 40px;
 
   position: relative;
@@ -11,5 +11,14 @@ export const ImageWrapper = styled.div`
   img {
     border-radius: 5px;
     object-fit: cover;
+  }
+
+  @media screen and (min-width: 1680px) {
+    margin: 40px auto;
+  }
+
+  @media screen and (max-width: 1024px) {
+    margin: 0;
+    margin-top: 40px;
   }
 `;
